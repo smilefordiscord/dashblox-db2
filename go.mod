dbebafea9976f16eebe22b2523d124310f7fba45
smile4 <bayoulionjr@gmail.com>
2024-10-24 00:57:47 -0800

    test

--- go.mod
+++ go.mod
@@ -1,5 +1,37 @@
 module github.com/digitalocean/sample-golang
 
-go 1.13
+go 1.21
 
-require github.com/gofrs/uuid v3.3.0+incompatible // indirect
+toolchain go1.23.2
+
+require (
+	github.com/creack/pty v1.1.9 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/pty v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240228155512-f48c80bd79b2 // indirect
+	golang.org/x/term v0.24.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/errgo.v2 v2.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
+	// "strconv"
 	"strings"
 
-	"github.com/gofrs/uuid"
+	// "github.com/gofrs/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 const startupMessage = `erm hello?`
@@ -16,75 +18,88 @@ func logRequest(r *http.Request) {
 	uri := r.RequestURI
 	method := r.Method
 	fmt.Println("Got request!", method, uri)
+	
 }
 
 func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		logRequest(r)
-		fmt.Fprintf(w, "Hello! i just farted everywhere so much farting you've requested %s\n", r.URL.Path)
+		fmt.Fprintf(w, "You should not be here... %s\n", r.URL.Path)
 	})
 
-	http.HandleFunc("/cached", func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-		maxAgeParams, ok := r.URL.Query()["max-age"]
-		if ok && len(maxAgeParams) > 0 {
-			maxAge, _ := strconv.Atoi(maxAgeParams[0])
-			w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", maxAge))
-		}
-		responseHeaderParams, ok := r.URL.Query()["headers"]
-		if ok {
-			for _, header := range responseHeaderParams {
-				h := strings.Split(header, ":")
-				w.Header().Set(h[0], strings.TrimSpace(h[1]))
-			}
-		}
-		statusCodeParams, ok := r.URL.Query()["status"]
-		if ok {
-			statusCode, _ := strconv.Atoi(statusCodeParams[0])
-			w.WriteHeader(statusCode)
-		}
-		requestID := uuid.Must(uuid.NewV4())
-		fmt.Fprint(w, requestID.String())
-	})
+	// http.HandleFunc("/cached", func(w http.ResponseWriter, r *http.Request) {
+	// 	logRequest(r)
+	// 	maxAgeParams, ok := r.URL.Query()["max-age"]
+	// 	if ok && len(maxAgeParams) > 0 {
+	// 		maxAge, _ := strconv.Atoi(maxAgeParams[0])
+	// 		w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", maxAge))
+	// 	}
+	// 	responseHeaderParams, ok := r.URL.Query()["headers"]
+	// 	if ok {
+	// 		for _, header := range responseHeaderParams {
+	// 			h := strings.Split(header, ":")
+	// 			w.Header().Set(h[0], strings.TrimSpace(h[1]))
+	// 		}
+	// 	}
+	// 	statusCodeParams, ok := r.URL.Query()["status"]
+	// 	if ok {
+	// 		statusCode, _ := strconv.Atoi(statusCodeParams[0])
+	// 		w.WriteHeader(statusCode)
+	// 	}
+	// 	requestID := uuid.Must(uuid.NewV4())
+	// 	fmt.Fprint(w, requestID.String())
+	// })
 
-	http.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-		keys, ok := r.URL.Query()["key"]
-		if ok && len(keys) > 0 {
-			fmt.Fprint(w, r.Header.Get(keys[0]))
-			return
-		}
-		headers := []string{}
-		headers = append(headers, fmt.Sprintf("host=%s", r.Host))
-		for key, values := range r.Header {
-			headers = append(headers, fmt.Sprintf("%s=%s", key, strings.Join(values, ",")))
-		}
-		fmt.Fprint(w, strings.Join(headers, "\n"))
-	})
+	// http.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+	// 	logRequest(r)
+	// 	keys, ok := r.URL.Query()["key"]
+	// 	if ok && len(keys) > 0 {
+	// 		fmt.Fprint(w, r.Header.Get(keys[0]))
+	// 		return
+	// 	}
+	// 	headers := []string{}
+	// 	headers = append(headers, fmt.Sprintf("host=%s", r.Host))
+	// 	for key, values := range r.Header {
+	// 		headers = append(headers, fmt.Sprintf("%s=%s", key, strings.Join(values, ",")))
+	// 	}
+	// 	fmt.Fprint(w, strings.Join(headers, "\n"))
+	// })
 
-	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-		keys, ok := r.URL.Query()["key"]
-		if ok && len(keys) > 0 {
-			fmt.Fprint(w, os.Getenv(keys[0]))
-			return
-		}
-		envs := []string{}
-		envs = append(envs, os.Environ()...)
-		fmt.Fprint(w, strings.Join(envs, "\n"))
-	})
+	// http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
+	// 	logRequest(r)
+	// 	keys, ok := r.URL.Query()["key"]
+	// 	if ok && len(keys) > 0 {
+	// 		fmt.Fprint(w, os.Getenv(keys[0]))
+	// 		return
+	// 	}
+	// 	envs := []string{}
+	// 	envs = append(envs, os.Environ()...)
+	// 	fmt.Fprint(w, strings.Join(envs, "\n"))
+	// })
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+	// http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+	// 	logRequest(r)
+	// 	codeParams, ok := r.URL.Query()["code"]
+	// 	if ok && len(codeParams) > 0 {
+	// 		statusCode, _ := strconv.Atoi(codeParams[0])
+	// 		if statusCode >= 200 && statusCode < 600 {
+	// 			w.WriteHeader(statusCode)
+	// 		}
+	// 	}
+	// 	requestID := uuid.Must(uuid.NewV4())
+	// 	fmt.Fprint(w, requestID.String())
+	// })
+
+	http.HandleFunc("/get-level", func(w http.ResponseWriter, r *http.Request) {
 		logRequest(r)
-		codeParams, ok := r.URL.Query()["code"]
-		if ok && len(codeParams) > 0 {
-			statusCode, _ := strconv.Atoi(codeParams[0])
-			if statusCode >= 200 && statusCode < 600 {
-				w.WriteHeader(statusCode)
-			}
-		}
-		requestID := uuid.Must(uuid.NewV4())
-		fmt.Fprint(w, requestID.String())
+		
 	})
 
 	port := os.Getenv("PORT")
